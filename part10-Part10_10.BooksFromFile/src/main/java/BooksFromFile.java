5eded70a805d6ad20811a4f89cfa3119eac4c55c
loop-forge <acmedia.france@gmail.com>
2025-06-23 00:23:53 +0200

    23-06-2025

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -11,7 +11,18 @@ public class BooksFromFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
-
+        readBooks(null)
     }
 
+    public static List<Book> readBooks(String file){
+        ArrayList<String> rows = new ArrayList<>();
+        ArrayList<Book> books = new ArrayList<>();
+        try {
+            Files.lines(Paths.get(file)).forEach(row -> rows.add(row));
+            rows.stream().forEach(row -> System.out.println("ok"));
+        } catch(Exception e){
+            System.out.println("Exception > " + e);
+        }
+        return books;
+    }
 }
